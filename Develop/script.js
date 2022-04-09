2427a9692f513c3257012a488bb297e029c567d2
Blake Smethers <smethersblake@gmail.com>
2022-04-09 10:08:02 -0500

    added timeStaus function for time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,45 @@
 var date = moment().format('dddd,MMM,Do YYYY')
 // console.log(date)
 $("#currentDay").text(date);
-
 // loads DOM then executes function
 $(document).ready(function () {
     $(".saveBtn").on("click", function(){
         var time = $(this).parent().attr("id")
-        console.log($(this).parent().attr("id"))
+        // console.log($(this).parent().attr("id"))
         var task = $(this).siblings(".description").val()
         // console.log(task)
         localStorage.setItem(time, task)
     })
-
 });
 var loadTasks = function() {
-    $("#9am .description").val(localStorage.getItem("9am"))
-    $("#10am .description").val(localStorage.getItem("10am"))
-    $("#11am .description").val(localStorage.getItem("11am"))
-    $("#12pm .description").val(localStorage.getItem("12pm"))
-    $("#1pm .description").val(localStorage.getItem("1pm"))
-    $("#2pmm .description").val(localStorage.getItem("2pm"))
-    $("#3pm .description").val(localStorage.getItem("3pm"))
-    $("#4pm .description").val(localStorage.getItem("4pm"))
-    $("#5pm .description").val(localStorage.getItem("5pm"))
+    $("#9 .description").val(localStorage.getItem("9"))
+    $("#10 .description").val(localStorage.getItem("10"))
+    $("#11 .description").val(localStorage.getItem("11"))
+    $("#12 .description").val(localStorage.getItem("12"))
+    $("#13 .description").val(localStorage.getItem("13"))
+    $("#14 .description").val(localStorage.getItem("14"))
+    $("#15 .description").val(localStorage.getItem("15"))
+    $("#16 .description").val(localStorage.getItem("16"))
+    $("#17 .description").val(localStorage.getItem("17"))
     
+}
+
+
+// var time = localStorage.key(0);
+// console.log(time)
+var timeStatus = function() {
+    var currentTime = moment().hour()
+    console.log(currentTime)
+    $(".time-block").each(function(){
+        var timeBlock = parseInt($(this).attr('id'))
+        if (timeBlock === currentTime) {
+            $(this).addClass("present")
         }
+        
+        console.log(timeBlock)
+    });
+}
 
-    
-    // var time = localStorage.key(0);
-    // console.log(time)
 
+timeStatus()
 loadTasks()
